b18278437e71e1bb44892351299bea39d5ef0e04
Howard Looman <hlooman@mix.wvu.edu>
2018-11-05 11:39:53 -0500

    Seeded Database

--- FHM/Data/Migrations/20181105163846_addGame.Designer.cs
+++ FHM/Data/Migrations/20181105163846_addGame.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FHM.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181105163846_addGame")]
+    partial class addGame
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FHM/Data/Migrations/20181105163846_addGame.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FHM.Data.Migrations
+{
+    public partial class addGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    GameID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GameImageThumbnailURL = table.Column<string>(nullable: true),
+                    GameImageUrl = table.Column<string>(nullable: true),
+                    GameIsGameOfTheWeek = table.Column<bool>(nullable: false),
+                    GameLongDescription = table.Column<string>(maxLength: 5000, nullable: false),
+                    GameName = table.Column<string>(maxLength: 100, nullable: false),
+                    GameShortDescription = table.Column<string>(maxLength: 5000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.GameID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}
