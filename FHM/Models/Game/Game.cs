27e94ff2b1a83dcbb276761e895608865736278d
Howard Looman <hlooman@mix.wvu.edu>
2018-11-05 10:56:00 -0500

    Added Game Model

---
+++ FHM/Models/Game/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FHM.Models.Game
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Game name is required")]
+        public string GameName { get; set; }
+        [Required]
+        [StringLength(5000, ErrorMessage = "Short Desc is required")]
+        public string GameShortDescription { get; set; }
+        [Required]
+        [StringLength(5000, ErrorMessage = "Long Desc is required")]
+        public string GameLongDescription { get; set; }
+        public string GameImageUrl { get; set; }
+        public string GameImageThumbnailURL { get; set; }
+        public bool GameIsGameOfTheWeek { get; set; }
+    }
+}
