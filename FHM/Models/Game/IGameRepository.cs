91be51361b17a1b36a3b5b4fbce9f5c19354e226
Howard Looman <hlooman@mix.wvu.edu>
2018-11-05 11:00:48 -0500

    Added IGameRepository

---
+++ FHM/Models/Game/IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FHM.Models.Game
+{
+    interface IGameRepository
+    {
+        IEnumerable<Game> GetAllGames();
+        Game GetGameByID(int gameID);
+        void AddGame(Game game);
+    }
+}
